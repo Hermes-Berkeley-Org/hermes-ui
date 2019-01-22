5d03c313d8003ce90950e6191c2e3449c9a37ef0
Zhuo Lu <sethlu@mintkit.net>
2019-01-21 23:01:42 -0800

    Redesign 404 & 403 pages

--- src/components/Forbidden.js
+++ src/components/Forbidden.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from './Layout.js';
+
+import './Forbidden.css';
 
 export default class Forbidden extends Component {
   render() {
     return (
-      <div>Forbidden</div>
+      <Layout>
+        <div className='container container-forbidden'>
+          <h1>🔒 You don't seem to have access to this page</h1>
+          <Link to='/'><span className='fa fa-arrow-left' /> Follow here to go back to home page</Link>
+        </div>
+      </Layout>
     );
   }
 }
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from './Layout.js';
+
+import './NotFound.css';
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div>Not found</div>
+      <Layout>
+        <div className='container container-not-found'>
+          <h1>🤔 Feeling like at the wrong place?</h1>
+          <Link to='/'><span className='fa fa-arrow-left' /> Follow here to go back to home page</Link>
+        </div>
+      </Layout>
     );
   }
 }
