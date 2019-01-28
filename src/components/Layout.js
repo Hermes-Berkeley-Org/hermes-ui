39d7503ce84d9ec0f35fb75e639b2c367d6f247a
Zhuo Lu <sethlu@mintkit.net>
2019-01-27 22:24:23 -0800

    Redesign the toasts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import '../utils/toast.css';
+
+class ToastCloseButton extends Component {
+  render() {
+    return <span className='fa fa-times' />;
+  }
+}
 
 class Layout extends Component {
   render() {
@@ -14,7 +22,11 @@ class Layout extends Component {
         <div className='layout-contents'>
           {this.props.children}
         </div>
-        <ToastContainer/>
+        <ToastContainer
+          closeButton={<ToastCloseButton />}
+          toastClassName='toast'
+          bodyClassName='toast-body'
+          progressClassName='toast-progress' />
         <Footer />
       </div>
     );
--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,26 +1,31 @@
 import { toast } from 'react-toastify';
-import { css } from 'glamor';
+
+if (!Array.prototype.random) {
+  Array.prototype.random = function () {
+    return this[Math.floor(Math.random() * this.length)];
+  };
+}
+
+function randomSuccessEmoji() {
+  return ['🌟', '🍩', '🎯', '🎉', '🎊', '🚀'].random()
+}
+
+function randomErrorEmoji() {
+  return ['🔔', '😢', '👀'].random()
+}
 
 export default {
-  success(msg, options = {}){
-    return toast.success(`🎉 ${msg}`, {
+  success(msg, options = {}) {
+    return toast(`${randomSuccessEmoji()} ${msg}`, {
       ...options,
       position: toast.POSITION.BOTTOM_RIGHT,
-      className: css({
-        backgroundColor: '#29bd9e',
-        fontFamily: 'Open Sans, Helvetica, Arial, sans-serif'
-      })
     });
   },
-  error(msg, options = {}){
-    return toast.error(`❌ ${msg}`, {
+  error(msg, options = {}) {
+    return toast(`${randomErrorEmoji()} ${msg}`, {
       ...options,
       position: toast.POSITION.BOTTOM_RIGHT,
-      className: css({
-        backgroundColor: '#ffffff',
-        color: '#000000',
-        fontFamily: 'Open Sans, Helvetica, Arial, sans-serif'
-      })
+      progressClassName: 'toast-progress toast-progress-error'
     });
   }
 }
